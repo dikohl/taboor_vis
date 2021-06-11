ef63e037f7cce2e0f58fa5734baf1723b3671504
Dimitri Kohler <gorotix@hotmail.com>
2021-06-11 13:55:08 +0200

    cheat sheet to png

---
+++ pages/cheatsheet.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
+
+export default function CheatSheet() {
+
+  return (    
+    <Layout>
+      <div className={styles.main}>
+        <h1>Taboor - Installation</h1>
+        <img src="/taboor.jpg" />
+      </div>
+    </Layout>
+  )
+}
